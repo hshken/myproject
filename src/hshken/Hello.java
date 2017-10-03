3144b59e0c3e2fc3067e7dd6a5496925b1ad39a6
Ken <hshken@livemail.tw>
2017-10-03 16:37:55 +0800

    first commit

---
+++ src/hshken/Hello.java
@@ -0,0 +1,10 @@
+package hshken;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	 System.out.println("Hello World");
+	 person p = new person();
+	 p.sayHello();
+	 }
+}
---
+++ src/hshken/person.java
@@ -0,0 +1,14 @@
+package hshken;
+
+public class person {
+
+	public void sayHello(){
+		System.out.println("Hello");}
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			 person p = new person();
+			 p.sayHello();
+
+	}
+
+}
