af08a1dc6a186e6d19809763975c28dc4f978774
hshken <32476286+hshken@users.noreply.github.com>
2017-10-15 23:29:12 +0800

    Update Hello.java

--- src/hshken/Hello.java
+++ src/hshken/Hello.java
@@ -3,7 +3,7 @@ package hshken;
 public class Hello {
 
 	public static void main(String[] args) {
-	 System.out.println("Hello World");
+	 System.out.println("Hello World haha");
 	 person p = new person();
 	 p.sayHello();
 	 }
