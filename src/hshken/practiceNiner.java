d42975eb87bdc0fdbddd0c32be397b75233aaee9
hshken <hshken@livemail.tw>
2017-11-04 23:41:00 +0800

    練習

---
+++ src/hshken/practiceNiner.java
@@ -0,0 +1,15 @@
+package hshken;
+
+public class practiceNiner {
+
+	public static void main(String[] args){
+		for (int n = 1 ;n <= 9 ; n++);{
+		    for(int i = 1;i <= 9;i++);{
+		       if ((n * i) >= 1) {
+		System.out.println(""+ n + "x" + i +" = " + (n * i)  + "\t");
+		}
+		System.out.println();
+	}
+}
+}
+}
