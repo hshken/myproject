87d8b529e7246cef79608316a7a3b113bd411544
hshken <hshken@livemail.tw>
2017-11-07 16:32:31 +0800

    variable, prmitive

--- src/hshken/prime.java
+++ src/hshken/prime.java
@@ -8,7 +8,7 @@ public class prime {
 		for(int n = 2; n<=1000 ;n++){
 			prime = true;
 			
-			for (int i = 1 ; n< i; i++){
+			for (int i = 2 ; n< i; i++){
 			
 		if( n % i == 0){
 				prime = false;
---
+++ src/hshken/test.java
@@ -0,0 +1,24 @@
+package hshken;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("誰的年紀最大?");
+		System.out.println("1)周杰倫");
+		System.out.println("2)華宸宇");
+		System.out.println("3)梁朝偉");
+		Scanner scanner =new Scanner(System.in);
+		String line = scanner.nextLine();
+		int number = Integer.parseInt(line);
+		if(number < 3){
+			System.out.println("3");
+		}else{
+			if(number = 3)
+			System.out.println("恭喜你答對了");
+
+	}
+
+}
+}
