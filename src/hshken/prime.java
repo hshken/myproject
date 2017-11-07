e332150d40a6d24c7b5e1b6fc23aaf8279581d84
hshken <hshken@livemail.tw>
2017-11-07 16:04:22 +0800

    variable, prmitive

--- src/hshken/prime.java
+++ src/hshken/prime.java
@@ -3,21 +3,22 @@ package hshken;
 public class prime {
 
 	public static void main(String[] args) {
-		boolean prime = true;
-		for(int n = 2; n<1000 ;n++)
-		for(int i = 2 ; n< i; i++){
-			(prime) = false;
-		if( n % i == 0){
+		boolean prime;
+		
+		for(int n = 2; n<=1000 ;n++){
+			prime = true;
 			
-			break;
+			for (int i = 1 ; n< i; i++){
+			
+		if( n % i == 0){
+				prime = false;
 			}
 		}
-		{
 			if (prime){
-			System.out.println();
-		}else{	
-			System.out.println("");		
+			System.out.print(n +" ");
 		}
+	
 		}
 }
-	}
+	
+}
