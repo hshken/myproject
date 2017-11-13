f5488c294b66b00d8a9b51eb43d566ef6e262140
hshken <hshken@livemail.tw>
2017-11-14 00:06:56 +0800

    test

--- src/hshken/prime.java
+++ src/hshken/prime.java
@@ -3,18 +3,17 @@ package hshken;
 public class prime {
 
 	public static void main(String[] args) {
-		boolean prime = true;
+		boolean prime;
 		for(int n = 2; n<=1000 ;n++) {
-			for (int i = 2 ; n< i; i++) {
-			if( n % i == 0) {
-				(prime) = false;
+			prime = true;
+			for (int i = 2 ; n > i; i++) {
+		        if((n % i) == 0){
+					(prime) = false;
 			}
-				}	
-		if (prime){
-			System.out.print(n +" ");
+				}
+			if (prime){
+				System.out.print( n +" ");
 		}
-	
-	
 }
 	}
 }
--- src/hshken/test2.java
+++ src/hshken/test2.java
@@ -1,12 +1,20 @@
 package hshken;
 
+import java.util.Scanner;
+
 public class test2 {
 
 	public static void main(String[] args) {
 		System.out.println("½Ð¿é¤J¼Æ¦r");
-		
-		System.out.println();
-
+		Scanner scanner = new Scanner(System.in);
+		 String line = scanner.nextLine();
+		 int star = Integer.parseInt(line);
+		 for (int n= 1; n <= star ; n++){
+				for (int i= 1; i <= star ; i++){
+					if(n>=i) 
+					System.out.print("*");
 	}
-
+				System.out.println("");
 }
+}
+	}
