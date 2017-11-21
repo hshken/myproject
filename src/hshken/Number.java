7e9aca7857b9ae3d741d4bd6bdc34595c700b7ec
hshken <hshken@livemail.tw>
2017-11-21 16:23:22 +0800

    Number

--- src/hshken/Number.java
+++ src/hshken/Number.java
@@ -1,9 +1,16 @@
 package hshken;
 
+import java.util.Scanner;
+
 public class Number {
 
 	public static void main(String[] args) {
-	for (int i = 1 ; i <=13 ; i++){
+		System.out.println("請輸入一個數字 ");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		int number = Integer.parseInt(line);
+		
+		for (int i = 1 ; i <= number ; i++){
 		if(i % 3 ==0){
 			continue;
 			}
