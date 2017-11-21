bc14bf42e6589ec674560318b757804536d80542
hshken <hshken@livemail.tw>
2017-11-21 16:20:04 +0800

    GameTester

---
+++ src/hshken/Number.java
@@ -0,0 +1,15 @@
+package hshken;
+
+public class Number {
+
+	public static void main(String[] args) {
+	for (int i = 1 ; i <=13 ; i++){
+		if(i % 3 ==0){
+			continue;
+			}
+		System.out.print(i + " ");
+	}
+
+	}
+
+}
