eacc87b9c27f9225a8563f116000259d665f7e5f
hshken <hshken@livemail.tw>
2017-11-28 16:26:32 +0800

    length

---
+++ src/hshken/Bubble.java
@@ -0,0 +1,20 @@
+package hshken;
+
+public class Bubble {
+
+	public static void main(String[] args) {
+		int n[] = {25,10,39,40,33,12};
+		for (int i=0; i <n.length; i++){
+			 if( n <= n[i]){
+		
+		int temp = a ;
+		a = b ;
+		b = temp;
+		for (int i : n){
+			System.out.println(i);
+
+	}
+			 }
+			 }
+		}
+	}
---
+++ src/hshken/Dice.java
@@ -0,0 +1,21 @@
+package hshken;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		int n1 ;
+		int n2 ;
+		int n3 ;
+		
+		int n[] = {4,5,6,1,2,3};
+//			n[0] = 4;
+//			n[1] = 5;
+//			n[2] = 6;
+//			for(int i = 0; i < n.length ; i++){
+//		System.out.println(n[i]);
+		for (int i : n){
+			System.out.println(i);
+	}
+
+}
+} 
