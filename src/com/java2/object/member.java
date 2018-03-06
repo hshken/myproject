8c6c3ba5b67d366ae3505b717d9b7e49247fc5a4
hshken <hshken@livemail.tw>
2018-03-06 16:27:36 +0800

    java1

--- src/com/java2/object/member.java
+++ src/com/java2/object/member.java
@@ -1,8 +1,15 @@
 package com.java2.object;
 
 public class member extends customer {
-	float discount = 0.1f;
-	private member (int price){
+	private float discount = 0.1f;
+	member (int price){
 		super(price);
 	}
+	
+	@Override
+	public void print(){
+		int total = (int)(price*0.9);
+		System.out.println(price+"\t"+total);
+		}
+	
 }
