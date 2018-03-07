0cbd49b33711bf84e1ea9adca29a04e5b7858008
hshken <hshken@livemail.tw>
2018-03-07 16:22:40 +0800

    java3

--- src/com/java2/object/customer.java
+++ src/com/java2/object/customer.java
@@ -1,15 +1,17 @@
 package com.java2.object;
 
-public class Customer {
-	public String name;
-	int price;
-	int IDnumber;
-	public Customer (int price){
-	this.price= price;
-	}
-	public void print(){
-		int total = (int)(price);
-		System.out.println(price+"\t"+total);
+	public class Customer {
+		public String name;
+		int price;
+		int IDnumber;
+		public Customer (int price){
+		this.price= price;
 		}
+		public void print(){
+			int total = (int)(price);
+			System.out.println(price+"\t"+total);
+			}
 
-}
+	}
+
+
--- src/com/java2/object/member.java
+++ src/com/java2/object/member.java
@@ -1,15 +1,17 @@
-package com.java2.object;
-public class Member extends Customer {
-	private float discount = 0.1f;
-	Member (int price){
-		super(price);
-	}
-	
-	@Override
-	public void print(){
-		int total = (int)(price*0.9);
-		System.out.println(price+"\t"+total);
+
+	package com.java2.object;
+	public class Member extends Customer {
+		private float discount = 0.1f;
+		Member (int price){
+			super(price);
 		}
-	
-}
+		
+		@Override
+		public void print(){
+			int total = (int)(price*0.9);
+			System.out.println(price+"\t"+total);
+			}
+		
+	}
+
 
