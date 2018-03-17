051e256e2f618f548b16a66810dfc7ebbbf5046a
hshken <hshken@livemail.tw>
2018-03-17 17:47:37 +0800

    ticket

--- src/com/java2/object/RoundtripTicket.java
+++ src/com/java2/object/RoundtripTicket.java
@@ -1,5 +1,9 @@
 package com.java2.object;
 
-public class RoundtripTicket {
-
+public class RoundtripTicket extends Ticket{
+	@Override
+	  public void print(){
+		  super.print();
+		  System.out.println(trainnumber +"\t"+
+				  arrive +"\t" + start+"\t"+price);			}
 }
--- src/com/java2/object/Ticket.java
+++ src/com/java2/object/Ticket.java
@@ -5,4 +5,18 @@ public class Ticket {
  int price;
  String arrive;
  String start;
+ public Ticket(String arrive , String start , int trainnumber , int price ){
+	 this.arrive = arrive;
+	 this.start = start;
+	 this.trainnumber = trainnumber;
+	 this.price = price;
+ }
+ public Ticket (int trainnumber){
+	 this.trainnumber =trainnumber;
+ }
+ public void print(){
+	System.out.println(trainnumber +"\t"+
+start +"\t" + arrive+"\t"+price);
+	}
 }
+
