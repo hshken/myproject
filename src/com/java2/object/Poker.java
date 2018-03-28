b659fd4d2d63f3c20fc89bc224baa7b15d820eaf
hshken <hshken@livemail.tw>
2018-03-28 17:45:02 +0800

    Poker2

--- src/com/java2/object/Poker.java
+++ src/com/java2/object/Poker.java
@@ -4,27 +4,27 @@ import java.util.Random;
 
 public class Poker {
 	Random random = new Random();
-	int[] cards = new int[52];
+	int[] poker = new int[52];
 	String flowers = "SHDC";
 	public Poker() {
-		for (int i=0 ; i < cards.length; i++) {
-			cards[i] = i;
-			System.out.println((i%13)+1+""+(flowers.charAt(i/13)));
+		for (int i=0 ; i < poker.length; i++) {
+			poker[i] = i;
+			System.out.println(((i%13)+1)+""+(flowers.charAt(i/13)));
 		}
 	}
 	public void shuffle() {
-		for (int i=0; i<cards.length; i++) {
+		for (int i=0; i<poker.length; i++) {
 			int a = random.nextInt(52);
-			int temp = cards[i];
-			cards[i] = cards[a];
-			cards[a] = temp;
+			int temp = poker[i];
+			poker[i] = poker[a];
+			poker[a] = temp;
 		}
 	}
 	public void show(){
-		for(int i=0; i<cards.length; i++) {
-			int c = cards[i];
-			System.out.print((c%13)+1+""+(flowers.charAt(c/13))+ " ");
-			if(i%13==0)
+		for(int i=0; i<poker.length; i++) {
+			int c = poker[i];
+			System.out.print((flowers.charAt(c/13))+""+((c%13)+1)+ " ");
+			if(i%13==12)
 				System.out.println();
 		}
 	}
--- src/com/java2/object/Poker2.java
+++ src/com/java2/object/Poker2.java
@@ -1,32 +1,27 @@
 package com.java2.object;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Poker2 {
-Random random = new Random();
-String flowers = "SHDC";
-ArrayList<Integer> cards = new ArrayList();
-public Poker2(){
-	for (int i=0 ; i < 52; i++) {
-		cards.size(i);
-		System.out.println((i%13)+1+""+(flowers.charAt(i/13)));
+	public static void main(String[]args) {
+	Random random = new Random();
+	String flowers = "SHDC";
+	List<Integer> cards = new ArrayList();
+	 for(int i=0 ; i < 52; i++) {
+		cards.add(i);
 }
 
-}
-public void shuffle() {
-	for (int i=0; i<cards.get; i++) {
+	for (int i=0; i<52; i++) {
 		int a = random.nextInt(52);
-		int temp = cards.add(i);
-		cards.get = cards.get;
-		cards.get(52) = temp;
+		int temp = cards.get(i);
+		cards.set(i,cards.get(a));
+		cards.set(a,temp);
 	}
-}
-public void show(){
-	for(int i=0; i<cards.; i++) {
-		int c = cards[i];
-		System.out.print((c%13)+1+""+(flowers.charAt(c/13))+ " ");
-		if(i%13==0)
+	for(int i=0; i<52; i++) {
+		System.out.print((flowers.charAt(cards.get(i)/13))+""+((cards.get(i)%13)+1)+ " ");
+		if(i%13==12)
 			System.out.println();
 	}
 }
