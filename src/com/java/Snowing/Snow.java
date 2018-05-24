761171c38e243135c9e161c247897fb920749f4e
hshken <hshken@livemail.tw>
2018-05-24 17:15:08 +0800

    Snow

--- src/com/java/Snowing/Snow.java
+++ src/com/java/Snowing/Snow.java
@@ -1,5 +1,28 @@
 package com.java.Snowing;
-
-public class Snow {
-
+import java.awt.Canvas;
+import java.util.Random;
+public class Snow extends Thread{
+	Canvas canvas;
+	Random random = new Random();
+	int x;
+	int y = 800;
+	public Snow() {
+		x = random.nextInt(600);
+	}
+	@Override
+	public void run() {
+		for(int i = 0; i < 700; i++) {
+			
+			
+			y = y + 1;
+			int movement = random.nextInt(3)-1;
+			x = x - movement;
+			try {
+				sleep(50);
+				canvas.repaint();
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/com/java/Snowing/SnowFrame.java
+++ src/com/java/Snowing/SnowFrame.java
@@ -1,10 +1,27 @@
 package com.java.Snowing;
 
-public class SnowFrame {
 
+import java.awt.Canvas;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+public class SnowFrame extends JFrame{
+	SnowCanvas canvas = new SnowCanvas();
+	public SnowFrame() {
+		setSize(620,850);
+		setLocation(200,200);
+		add(canvas);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new SnowFrame();
 
 	}
-
+	public class SnowCanvas extends Canvas{
+		List<Snow> snows = new ArrayList<>();
+		
+	}
 }
