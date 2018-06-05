e2f5e83c7a88292a5e6fc6730529bd17e008ea0f
hshken <hshken@livemail.tw>
2018-06-05 16:29:35 +0800

    Vending

--- src/com/java2/VendingMain/VendingMain.java
+++ src/com/java2/VendingMain/VendingMain.java
@@ -1,11 +1,9 @@
 package com.java2.VendingMain;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import com.java2.io.Sales;
 
@@ -13,15 +11,15 @@ public class VendingMain {
 
 	public static void main(String[] args) {
 		VendingMachine vm = new VendingMachine();
-		FileReader rd;
 		try {
-	rd	 = new FileReader("Vending.txt");
+	FileReader rd	 = new FileReader("Vending.txt");
 	BufferedReader in = new BufferedReader(rd);
 	String line = in.readLine();
-		String[] tokens = line.split(",");
+	String[] tokens = line.split(",");
 		for(int i=0 ; i<= 3;i++){
 			String name = (tokens[i*2+1]);
 			int price = Integer.parseInt(tokens[i*2+2]);
+			vm
 		}		
 } 
 		catch (FileNotFoundException e) {
