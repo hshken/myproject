4074a24fa1e465420eb271f53729437c04b94040
hshken <hshken@livemail.tw>
2018-06-07 17:21:27 +0800

    Vendinghome

--- src/com/java2/VendingMain/VendingMachine.java
+++ src/com/java2/VendingMain/VendingMachine.java
@@ -2,13 +2,19 @@ package com.java2.VendingMain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
 	int total;
 	List<Drink> drinks = new ArrayList<>();
 	
 	public void start(){
-		System.out.println("Please insert coins 5¤¸,10¤¸,50¤¸");
-		
+		System.out.println("Please insert coins 5å…ƒ,10å…ƒ,50å…ƒ");
+		for(int i=0;i<drinks.size(); i++) {
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t"+ drink.getName()+"\t"+drink.getPrice());
+		}
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
 	}
 }
