1305da66c78e93d53592ac0942b15d3482b6f67c
hshken <hshken@livemail.tw>
2018-06-09 17:25:59 +0800

    mazeGame

--- src/com/java2/Game/Game.java
+++ src/com/java2/Game/Game.java
@@ -1,5 +1,21 @@
 package com.java2.Game;
 
-public class Game {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-}
+public class Game {
+	public Game() {
+		try {
+			FileReader rd= new FileReader("maze.txt");
+	BufferedReader bfr = new BufferedReader(rd);
+	String line = bfr.readLine();
+	String[] steps = line.split(",");
+	int wid = Integer.parseInt(steps[0]);
+	int lon = Integer.parseInt(steps[1]);
+	
+		}
+	}
+	
+	
+	
+	}
