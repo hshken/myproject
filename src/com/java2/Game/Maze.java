122119fc982cb40a7ccf5d43e18fd171daf96f28
hshken <hshken@livemail.tw>
2018-06-10 11:46:27 +0800

    mazeGame2

--- src/com/java2/Game/Maze.java
+++ src/com/java2/Game/Maze.java
@@ -1,5 +1,14 @@
 package com.java2.Game;
 
 public class Maze {
-
+	int row;
+	int col;
+	int snareCount;
+	public Maze(int row,int col,int snareCount) {
+		this.row = row;
+		this.col = col;
+		this.snareCount = snareCount;
+		
+	}
+	
 }
