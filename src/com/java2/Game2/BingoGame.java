1143a2fb18d4e775be160ef56c6d4aeea9b18e4d
hshken <hshken@livemail.tw>
2018-06-11 23:30:20 +0800

    20180611

--- src/com/java2/Game2/BingoGame.java
+++ src/com/java2/Game2/BingoGame.java
@@ -1,13 +1,17 @@
 package com.java2.Game2;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import com.java.Snowing.Snow;
+import com.java2.Game2.Bingo.Form;
 
-public class BingoGame {
+public class BingoGame extends Bingo{
+	public BingoGame() {
 	try {
 		FileReader rd	 = new FileReader("Bingo.txt");
 	BufferedReader in = new BufferedReader(rd);
@@ -16,14 +20,24 @@ public class BingoGame {
 	int table = (int)Math.sqrt(number);
 	line = in.readLine();
 	String[] tokens = line.split(",");
-	List<Integer> bingo = new ArrayList<>();
-	for (int i=0; i<=tokens.length; i++) {
-		int bingo = Integer.parseInt(tokens[i]);
-		Bingo bingo = new bingo(this);
-		bingo.start();
+	List<Integer> bingos = new ArrayList<>();
+	for (int i=0; i<=tokens.length-1; i++) {
+		int bingo= Integer.parseInt(tokens[i]);
 		bingos.add(bingo);
 		
 	}
+	Form form= new Form(table, number, bingos);
+	form.print();
+	form.Bingo();
 	
-	}
+} catch (FileNotFoundException e) {
+	e.printStackTrace();
+} catch (IOException e) {
+	e.printStackTrace();
+}
+}
+
+public static void main(String[] args) {
+	new BingoGame();
+}
 }
